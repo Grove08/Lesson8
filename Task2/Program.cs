9f6079f5c1b766575aca9747551d61cab9301b79
Артём Белоусов <belousov@psksk.ru>
2023-11-01 12:43:20 +0300

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Temp\Numbers.txt";
+            Random random = new Random();
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(random.Next(-10, 10));
+                }
+            }
+            int k = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    int number = Convert.ToInt32(sr.ReadLine());
+                    k += number;
+                }
+            }
+            Console.WriteLine("Сумма случайных чисел {0}", k);
+            Console.ReadKey();
+        }
+    }
+}
